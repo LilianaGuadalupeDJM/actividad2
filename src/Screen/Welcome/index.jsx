f3c67e0da6c0f108b6760af9086955c9371f6e45
Liliana25072003 <lilianadejesusmartinez3@gmail.com>
2024-05-21 12:13:36 -0600

    feat(perfil): agregar componente Profile y actualizar Welcome

--- src/Screen/Welcome/index.jsx
+++ src/Screen/Welcome/index.jsx
@@ -1,12 +1,19 @@
 // src/Screen/Welcome/index.jsx
 import React from 'react';
-import './welcome.css';
+import { useHistory } from 'react-router-dom'; // Importa useHistory para la navegación
 
 const Welcome = () => {
+  const history = useHistory();
+
+  const goToProfile = () => {
+    history.push('/profile'); // Navega a la ruta '/profile' al hacer clic en el botón
+  };
+
   return (
-    <div className="welcome-container">
+    <div>
       <h1>Bienvenido</h1>
-      <p>Esta es la pantalla de bienvenida. </p>
+      <p>Esta es la pantalla de bienvenida.</p>
+      <button onClick={goToProfile}>Ir a Perfil</button> {/* Agrega un botón para ir a la pantalla de perfil */}
     </div>
   );
 };
---
+++ src/Screen/perfil/perfil.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+const Perfil = () => {
+  return (
+    <div>
+      <h1>Perfil de Usuario</h1>
+      <p>Aquí puedes ver y editar tu perfil.</p>
+    </div>
+  );
+};
+
+export default Perfil;
