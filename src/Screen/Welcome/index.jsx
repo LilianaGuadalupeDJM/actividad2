9ad5b3151b5ab6587da88b1fb72924bb86757f0b
Liliana25072003 <lilianadejesusmartinez3@gmail.com>
2024-05-20 21:29:22 -0600

    primer cambio archivo index.jsx

---
+++ src/Screen/Welcome/index.jsx
@@ -0,0 +1,14 @@
+// src/Screen/Welcome/index.jsx
+import React from 'react';
+import './welcome.css';
+
+const Welcome = () => {
+  return (
+    <div className="welcome-container">
+      <h1>Bienvenido</h1>
+      <p>Esta es la pantalla de bienvenida. </p>
+    </div>
+  );
+};
+
+export default Welcome;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Welcome from './Screen/Welcome';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Welcome />
+  </React.StrictMode>
+);
